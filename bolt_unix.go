aa6732a6e66ccd06eaf507a9a0061308f35588c2
Naoki Takano <honten@tinkermode.com>
2018-02-21 21:28:25 -0800

    Check error code for madvise

--- bolt_unix.go
+++ bolt_unix.go
@@ -53,7 +53,10 @@ func mmap(db *DB, sz int) error {
 	}
 
 	// Advise the kernel that the mmap is accessed randomly.
-	if err := madvise(b, syscall.MADV_RANDOM); err != nil {
+	err = madvise(b, syscall.MADV_RANDOM)
+	if err != syscall.ENOSYS {
+		// The syscall is not implemented in kernel but it still works.
+	} else if err != nil {
 		return fmt.Errorf("madvise: %s", err)
 	}
 
